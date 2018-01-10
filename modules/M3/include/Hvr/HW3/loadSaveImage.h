5d81b2bad2901d9ef1e4dacda426ecad7b9030d9
Richard Yao <richard823@gmail.com>
2018-01-10 02:58:59 -0800

    Reformated structure with only HW3

---
+++ modules/M3/include/Hvr/HW3/loadSaveImage.h
@@ -0,0 +1,31 @@
+#ifndef LOADSAVEIMAGE_H__
+#define LOADSAVEIMAGE_H__
+
+#include <cuda_runtime.h>  //for uchar4
+#include <string>
+
+M3_DLL
+void loadImageHDR(const std::string &filename,
+                  float **imagePtr,
+                  size_t *numRows,
+                  size_t *numCols);
+
+M3_DLL
+void loadImageRGBA(const std::string &filename,
+                   uchar4 **imagePtr,
+                   size_t *numRows,
+                   size_t *numCols);
+
+M3_DLL
+void saveImageRGBA(const uchar4 *const image,
+                   const size_t numRows,
+                   const size_t numCols,
+                   const std::string &output_file);
+
+M3_DLL
+void saveImageHDR(const float *const image,
+                  const size_t numRows,
+                  const size_t numCols,
+                  const std::string &output_file);
+
+#endif
